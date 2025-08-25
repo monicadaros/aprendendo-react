6618aa395f60c4b2ea869235509e03d33f739b91
Monica <monicac2807@gmail.com>
2025-08-25 09:16:18 -0300

    Evento e e.preventDefault

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,15 @@
 import './App.css';
-import HelloWord from './components/HelloWord';
-import Pessoas from './components/Pessoas';
-import Frase from './components/Frase';
-import List from './components/List';
+import Evento from './components/Evento';
+import Form from './components/Form';
 
 function App() {
   const nome = "Maria"
   return (
     <div className="App">
-      <HelloWord />
-      <Frase />
-      <Frase />
-      <List/>
-      <Pessoas 
-        nome="Monica" idade="32" profissao="Professora" 
-      />
+      <h1>Testando Eventos</h1>
+      <Evento numero="1"/>
+      <Evento numero="2"/>
+      <Form />
     </div>
   );
 }
---
+++ my-app/src/components/Form.js
@@ -0,0 +1,24 @@
+function Form () {
+
+    function cadastrarUsuario (e){
+        e.preventDefault()
+        console.log("Cadastrou")
+    }
+
+    return (
+        <>
+        <h1>Meu cadastro</h1>
+        <form onSubmit={cadastrarUsuario}>
+            <div>
+                <input type="text" placeholder="Digite seu nome" /> 
+            </div>
+            <div>
+                <input type="submit" value="Cadastrar"/>
+
+            </div>
+        </form>
+        </>
+    )
+}
+
+export default Form
