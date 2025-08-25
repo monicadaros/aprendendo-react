3d7e7e48ec50fb41135bfe3fffec51e102182b56
Monica <monicac2807@gmail.com>
2025-08-25 08:48:56 -0300

    props

--- my-app/src/components/List.js
+++ my-app/src/components/List.js
@@ -5,8 +5,9 @@ function List (){
         <>
             <h1>Minha Lista</h1>
             <ul>
-                <Item marca="Ford"/>
-                <Item marca="Fiat"/>
+                <Item marca="Ford" ano_lancamento={2010}/>
+                <Item marca="Fiat" ano_lancamento={2022}/>
+                <Item marca="Citroen" ano_lancamento={2005}/>
             </ul>
         </>
     )
